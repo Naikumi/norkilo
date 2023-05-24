602d2c24bcc8521f3cadda74a4e4923607347b26
Naikumi <naikumimartha@gmail.com>
2023-05-24 13:11:06 -0700

    changes

--- simple_trial/shell_01.c
+++ simple_trial/shell_01.c
@@ -1,117 +1,117 @@
 #include "functions.h"
 
-void prompt_and_read_input(char **cont, ssize_t *c, int val_fd)
+void prompt_and_read_input(char **con, ssize_t *ch, int value_fd)
 {
-    if (val_fd != 0)
-        write(1, "#cisfun$ ", 10);
+	if (value_fd != 0)
+	write(1, "#cisfun$ ", 10);
 
-    *cont = read_line(c);
+	*con = read_line(c);
 
-    if (*c == EOF)
-    {
-        free(*cont);
-        write(1, "\n", 2);
-        exit(3);
-    }
+	if (*ch == EOF)
+	{
+		free(*con);
+		write(1, "\n", 2);
+		exit(3);
+	}
 
-    if (**cont == '\n')
-    {
-        free(*cont);
-        return;
-    }
+	if (**con == '\n')
+	{
+		free(*con);
+		return;
+	}
 }
 
-void handle_built_in_commands(char **tokenizado, char **env)
+void handle_built_in_commands(char **tokenizado, char **environ)
 {
-    int comp = _strcmp(tokenizado[0], "exit");
+	int comp = _strcmp(tokenizado[0], "exit");
 
-    if (comp == 0 && tokenizado[1] == NULL)
-    {
-        free(tokenizado);
-        exit(3);
-    }
+	if (comp == 0 && tokenizado[1] == NULL)
+	{
+		free(tokenizado);
+		exit(3);
+	}
 
-    comp = _strcmp(tokenizado[0], "env");
+	comp = _strcmp(tokenizado[0], "env");
 
-    if (comp == 0)
-    {
-        int i = 0;
-        while (env[i] != NULL)
-        {
-            write(1, env[i], _strlen(env[i]));
-            write(1, "\n", 1);
-            i++;
-        }
-    }
+	if (comp == 0)
+	{
+		int j = 0;
+
+		while (environ[j] != NULL)
+		{
+			write(1, environ[j], _strlen(environ[j]));
+			write(1, "\n", 1);
+			j++;
+		}
+	}
 }
 
-void execute_child_process(int argc, char *argv[], char **env)
+void execute_child_process(int argc, char *argv[], char **environ)
 {
-    char *cont, *result;
-    char **tokenizado;
-    ssize_t c;
-    int val_fd = 0;
+	char *con, *result;
+	char **tokenizado;
+	ssize_t ch;
+	int value_fd = 0;
 
-    val_fd = isatty(STDIN_FILENO);
+	value_fd = isatty(STDIN_FILENO);
 
-    if (argc == 1)
-    {
-        prompt_and_read_input(&cont, &c, val_fd);
+	if (argc == 1)
+	{
+		prompt_and_read_input(&con, &ch, value_fd);
+		if (con == NULL)
+			return;
 
-        if (cont == NULL)
-            return;
+	tokenizado = words(con, " \t\r\n");
 
-        tokenizado = words(cont, " \t\r\n");
+	handle_built_in_commands(tokenizado, environ);
 
-        handle_built_in_commands(tokenizado, env);
+	result = l_path(tokenizado[0], environ);
 
-        result = l_path(tokenizado[0], env);
-
-        if (execve(result, tokenizado, NULL) == -1)
-        {
-            free(result);
-            free(tokenizado);
-            perror(argv[0]);
-	    exit(EXIT_FAILURE);
-            if (val_fd == 0)
-                exit(2);
-            return;
-        }
-    }
+	if (execve(result, tokenizado, NULL) == -1)
+	{
+		free(result);
+		free(tokenizado);
+		perror(argv[0]);
+		exit(EXIT_FAILURE);
+		if (value_fd == 0)
+			exit(2);
+		return;
+	}
+	}
 }
 
-int main(int argc, char *argv[], char **env)
+int main(int argc, char *argv[], char **environ)
 {
-    int status = 0, val_fd = 0;
-    pid_t hijo;
+	int status = 0, value_fd = 0;
+	pid_t child;
 
-    while (1)
-    {
-        hijo = fork();
-        if (hijo == -1)
-        {
-            perror("Error: hijo");
-            return -1;
-        }
+	while (1)
+	{
+	child = fork();
+	if (child == -1)
+	{
+	perror("Error: child");
+	return (-1);
+	}
 
-        val_fd = isatty(STDIN_FILENO);
+	value_fd = isatty(STDIN_FILENO);
 
-        if (hijo == 0)
-        {
-            execute_child_process(argc, argv, env);
-        }
-        else
-        {
-            wait(&status);
-            status = WEXITSTATUS(status);
-            if (status == 2)
-                exit(127);
-            if (val_fd == 0 || status == 3)
-            {
-                break;
-            }
-        }
-    }
+	if (child == 0)
+	{
+	execute_child_process(argc, argv, environ);
+	}
+	else
+	{
+	wait(&status);
+	status = WEXITSTATUS(status);
+	if (status == 2)
+	exit(127);
+	if (value_fd == 0 || status == 3)
+	{
+	break;
+	}
+	}
+	}
 
-    return 0;
+	return (0);
 }
